7abb9d04ebd7169387de6065fdebe1cc5b3d6ebc
jihwan <jihwan0713@naver.com>
2026-02-11 17:01:20 +0900

    First init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+
+const userRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const likesRouter = require('./routes/likes');
+const ordersRouter = require('./routes/orders');
+const cartsRouter = require('./routes/carts');
+
+app.use("/users", userRouter);
+app.use("/books", booksRouter);
+app.use("/likes", likesRouter);
+app.use("/carts", cartsRouter);
+app.use("/orders", ordersRouter);
