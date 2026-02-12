1d9a28f1f30cc72fff7027a0f776b6d4ac241181
jihwan <jihwan0713@naver.com>
2026-02-12 21:33:04 +0900

    2/12 스터디

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,18 @@
-const express = require('express');
+const express = require('express'); // express 모듈
 const router = express.Router();
+const conn = require('../mariadb'); // db 모듈
 
-// 회원가입
-router.post('/join', (req,res) => {
-    res.json('회원가입')
-});
+const {join, login, passwordReset, passwordResetRequest} = require('../controller/UserController')
 
-// 로그인
-router.post('/login', (req,res) => {
-    res.json('로그인')
-});
+router.use(express.json());
 
-// 비밀번호 초기화 요청
-router.post('/reset', (req,res) => {
-    res.json('비밀번호 초기화 요청')
-});
+// const { Result } = require('express-validator');
 
-// 비밀번호 초기화
-router.put('/reset', (req,res) => {
-    res.json('비밀번호 초기화')
-});
+
+router.post('/join',join); // 회원가입
+router.post('/login',login); // 로그인
+router.put('/reset',passwordReset); // 비밀번호 초기화 요청
+router.post('/reset',passwordResetRequest); // 비밀번호 초기화
 
 
 module.exports = router;
